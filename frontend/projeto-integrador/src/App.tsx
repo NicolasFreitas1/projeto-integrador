70b287dccc3ce83cf6b6f49d5e0fc85ca0564f51
llucaslopes <phyngo,lucas@hotmail.com>
2024-10-04 15:37:30 -0300

    feat: :sparkles: impremantando back nos outros componentes

--- frontend/projeto-integrador/src/App.tsx
+++ frontend/projeto-integrador/src/App.tsx
@@ -1,7 +1,7 @@
 import Login from "./components/Login/Login";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Products from "./components/Products/Products";
-import Stock from "./components/Stock/Stock";
+import AddProduct from "./components/AddProduct/AddProduct";
+import EditProduct from "./components/EditProduct/EditProduct";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -13,6 +13,8 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-product" element={<AddProduct />} />
+          <Route path="/edit-product/:barcode" element={<EditProduct />} />
         </Routes>
       </Router>
     </div>
